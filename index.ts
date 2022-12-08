290a9b0a7bfa7c24565bf2b6a5ba9d009a421d0d
Alex Aylwin <aaylwin@amazon.com>
2022-12-08 08:55:23 -0500

    Add day 3 solution

--- index.ts
+++ index.ts
@@ -7,58 +7,95 @@ const l = (s: any) => console.log(s);
 
 /** --- End Utilities and Set Up */
 
-const roundScore = (y: string, t: string): number => {
-  let score = 0;
-  
-  //table for win/loss/tie
-  if(t == 'A' && y == 'X') { score = 3} else if
-  (t == 'A' && y == 'Y' ) { score = 6 } else if
-  (t == 'A' && y == 'Z' ) { score = 0 } else if
+/** Part One */
 
-  (t == 'B' && y == 'X' ) { score = 0 } else if
-  (t == 'B' && y == 'Y' ) { score = 3 } else if
-  (t == 'B' && y == 'Z' ) { score = 6 } else if
+const getCompartments = (sack: string): string[] => 
+  [ sack.substring(0, sack.length / 2),
+  sack.substring(sack.length / 2) ]
 
-  (t == 'C' && y == 'X' ) { score = 6 } else if
-  (t == 'C' && y == 'Y' ) { score = 0 } else if
-  (t == 'C' && y == 'Z' ) { score = 3 };
-
-  if(y == 'X') { score = score + 1} else if
-  (y == 'Y') { score = score + 2 } else if
-  (y == 'Z') { score = score + 3 }
-  
-  return score;
+const findSharedChar = (strings: string[]): string => {
+  let charArrays: string[][] = strings.map( (v) => v.split(''));
+  let res = '';
+  charArrays[0].forEach( (c) => {
+    charArrays.map( (ary) => ary.indexOf(c) ).filter( (v) => v > -1 ).length == charArrays.length ? res = c : res = res;
+  })
+  return res;
 }
 
-const partOne = (input: string[]): number => {
-  
-  return input.reduce(
-    (acc, curr) => acc + roundScore(curr.split(' ')[1], curr.split(' ')[0])
-  , 0)
+const calcPriority = (item: string): number => 
+item.charCodeAt(0) > 90 ? item.charCodeAt(0) - 96 : item.charCodeAt(0) - 38
+
+/** Tests */
+const runTests = (): void => {
+  let errs: string[] = [];
+  /** getCompartments */
+  input.forEach( (sack: string) => {
+    const res = getCompartments(sack);
+    if(res[0].length != res[1].length) {
+      errs.push("Error - getCompartments length");
+    }
+  });
+
+  /** findSharedItem */
+  input.forEach( (sack: string, i) => {
+    const c = getCompartments(sack);
+    const res = findSharedChar(c);
+    switch (i) {
+      case 0:
+        if(res != 'p') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;
+      case 1:
+        if(res != 'L') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;
+      case 2:
+        if(res != 'P') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;
+      case 3:
+        if(res != 'v') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;
+      case 4:
+        if(res != 't') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;
+      case 5:
+        if(res != 's') errs.push('Error - findSharedItem 0 returned ' + res);
+        break;      
+      }
+  })
+  //Print results
+  if( errs.length > 0 ) {
+    l("Errors occurred:")
+    errs.forEach( (e) => l(e) );
+  } else {
+    l("No errors");
+  }
 }
 
-l(partOne(input));
+//runTests();
 
-const chooseAction = (t: string, outcome: string): string =>  {
-  const nt = t.charCodeAt(0);
-  let y = 0;
-  if(outcome == 'X') { y = nt - 1} else if
-  (outcome == 'Y') { y = nt } else if
-  (outcome == 'Z') { y = nt + 1 }
+// let res = input.reduce(
+//   (acc, curr) => {
+//     let priority = calcPriority(findSharedItem(getCompartments(curr)));
+//     return acc + priority;
+//   }
+// , 0)
 
-  if(y > 67) { y = 65 }
-  if(y < 65 ) { y = 67 }
-  
-  y = y + 23
-  //l(outcome + ' ' + t + ' ' + String.fromCharCode(y));
-  return String.fromCharCode(y);
+
+/** PART TWO */
+
+const getElfGroups = (input: string[]): string[][] => {
+  let res:string[][] = [];
+  let curr:string[] = [];
+  input.forEach( (v, i) => {
+    curr.push(v);
+    if(i > 0 && (i+1) % 3 == 0) {
+      res.push(curr);
+      curr = [];
+    }
+  })
+
+  return res;
 }
-
-const partTwo = (input: string[]): number => {
-  return input.reduce(
-    (acc, curr) => acc + roundScore(chooseAction(curr.split(' ')[0], curr.split(' ')[1]), curr.split(' ')[0]),
-    0
-  )
-}
-
-l(partTwo(input));
+l('---')
+l(getElfGroups(input).map((g) => findSharedChar(g)).map( (item) => calcPriority(item) ).reduce(
+  (acc, curr) => acc + curr, 0
+));
